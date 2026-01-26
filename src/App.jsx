b677dc7c1f2ed7c56004afaa459617e719f65d43
trio <ashuubayeva_g@iuca.kg>
2026-01-26 19:02:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import DataDisplay from "./components/DataDisplay";
+
+function App(){
+  return(
+    <>
+    
+    <Header/>
+    <Home />
+    <DataDisplay />
+    <Footer />
+
+    </>
+  )
+}
+export default App
